0e67778e72c24c23a32e3bf5410bb5dde319e2e1
penrose0214 <penrose0214@gmail.com>
2026-01-26 17:06:09 +0900

    fix: embed.py import 구문 수정

--- rag/src/infrastructure/ingestion/embed.py
+++ rag/src/infrastructure/ingestion/embed.py
@@ -1,7 +1,7 @@
 import logging
 from typing import List
 
-from langchain_huggingface import HuggingFaceBgeEmbeddings
+from langchain.embeddings import HuggingFaceBgeEmbeddings
 
 from config import settings
 
