cc5a92d220390869e53de43418aa55af7bf51a85
penrose0214 <penrose0214@gmail.com>
2026-02-03 12:48:07 +0900

    update: PDF-to-text 분리로 인한 setup.sh 파일 분리

--- setup.sh
+++ setup.sh
@@ -1,59 +1,31 @@
 #!/bin/bash
 
-echo "1. 시스템 패키지 업데이트 및 필수 도구 설치"
+echo "1. 시스템 패키지 업데이트 및 필수 도구 설치 (RAG)"
 sudo apt update -y
 sudo apt install -y \
     python3 \
     python3-pip \
     python3-venv \
-    poppler-utils \
     libmariadb-dev \
+    gcc \
+    g++ \
+    pkg-config \
     git \
     curl \
     unzip \
     build-essential
 
 echo "2. 프로젝트용 Python 가상환경 생성"
-python3 -m venv venv
-. ./venv/bin/activate
+python3 -m venv .venv
+. ./.venv/bin/activate
 
-echo "3. Python 패키지 의존성 설치"
+echo "3. Python 패키지 의존성 설치 (rag)"
 pip install --upgrade pip
-
-# 설치할 패키지 리스트
-REQUIREMENTS=$(cat <<EOF
-fastapi
-uvicorn
-pydantic
-requests
-python-dotenv
-Pillow
-langchain
-langchain-community
-langchain-huggingface
-huggingface_hub
-sentence-transformers
-qdrant-client
-PyMuPDF
-openai
-google-genai
-MariaDB
-sqlalchemy
-python-multipart
-fastapi-pagination
-tiktoken
-EOF
-)
-
-# requirement를 모두 text로 작성
-echo "$REQUIREMENTS" > requirements.txt
-pip install -r requirements.txt
+pip install -r rag/requirements.txt
 
 echo "모든 Python 패키지 설치 완료"
 
-echo "4. 테스트용 "
-
-echo "5. FastAPI 서버 실행 예시 (기본 uvicorn)"
-echo "source venv/bin/activate && uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload"
+echo "4. FastAPI 서버 실행 예시 (RAG)"
+echo "source .venv/bin/activate && cd rag/src && uvicorn main:app --host 0.0.0.0 --port 8000 --reload"
 
 echo "전체 설정 완료!"
